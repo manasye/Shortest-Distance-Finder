4e23a827f4c828d47cbf3538f863005e73a645e7
Antonio Setya <antonio_setya@hotmail.com>
2018-04-01 10:36:05 +0700

    POST method at /ProcInput
    Codes for testing POST with AJAX is still there in index.html.

--- main.py
+++ main.py
@@ -9,13 +9,35 @@
     """
 
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__) # The web server
+adj_matrix = [];
+
+""" A* Algorithm """
+def AStar():
+    path = []
+    # Process adj_matrix here
+    print("nanti ya, wkwk")
+    return path
 
 """ ROUTING RULES """
+# Main page
 @app.route("/",methods=["GET"])
 def SendMain():
     return render_template("index.html")
+
+# Send input to this address to be processed
+@app.route("/ProcInput",methods=["POST"])
+def ProcessInput():
+    try:
+        out = "HAHAHAHAHA\nYou send this : " + request.form["test"] + "."
+    except(KeyError):
+        out = "HAHAHAHAHA\nYou didn't send a thing."
+    # Put input into adj_matrix
+    # out = AStar();
+    # Format out as JSON string
+    return out
 
 # Run the server
 if (__name__) == '__main__':
